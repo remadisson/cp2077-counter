aef00112b3a3b42f01b68b9a4252d5a354fa5d24
remadisson <maximilianloers@icloud.com>
2020-08-31 18:22:26 +0200

    Improvements

--- modules/twitter.js
+++ modules/twitter.js
@@ -1,44 +1,95 @@
-const dotenv = require('dotenv');
-const twitter = require('twitter-lite');
-const files = require('./files');
+const dotenv = require("dotenv");
+const twitter = require("twitter-lite");
+const files = require("./files");
 dotenv.config();
 let env = process.env;
 
-
 const client = new twitter({
-    consumer_key: env.consumer_key,  
-    consumer_secret: env.consumer_secret,  
-    access_token_key: env.access_token_key,  
-    access_token_secret: env.access_token_secret  
+  consumer_key: env.consumer_key,
+  consumer_secret: env.consumer_secret,
+  access_token_key: env.access_token_key,
+  access_token_secret: env.access_token_secret,
 });
 
-module.exports.sendTweet = (message) => {
-    let lastsend = process.env.lastsend;
-    
-    let newdate = new Date();
-    let lastdate = getType(lastsend) !== undefined ? new Date(lastsend) : undefined;
-    
-    if(lastdate == undefined ? false : (newdate.getDate() == lastdate.getDate())){
-        return "Last send is today. (Last: " + lastdate.getDate() + " == " + newdate.getDate() + ")";
+module.exports.sendTweet = (distance, message) => {
+  let d = distance[0];
+  let unit = distance[1];
+
+  let lastsend = process.env.lastsend;
+
+  let newdate = new Date();
+
+  if(lastsend == "finished"){
+      return "Already done! Please shut me down!";
+  }
+
+  let lastdate =
+    getType(lastsend) !== undefined ? new Date(lastsend) : undefined;
+
+  if (lastdate == undefined ? false : newdate.getDate() == lastdate.getDate()) {
+    return (
+      "Last send is today. (Last: " +
+      lastdate.getDate() +
+      " == " +
+      newdate.getDate() +
+      ")"
+    );
+  }
+
+  switch (unit.toString().toLowerCase()) {
+    case "days": {
+      if (newdate.getHours() != 10 && lastdate != undefined) {
+        return (
+          "Last send is " +
+          lastdate.getDate() +
+          "/" +
+          lastdate.getMonth() +
+          "/" +
+          lastdate.getFullYear()
+        );
+      }
     }
 
-    if(newdate.getHours() != 10 && lastdate != undefined){
-        return "Last send is " + lastdate.getDate() + "/" + lastdate.getMonth() + "/" + lastdate.getFullYear();
+    case "hours": {
+        if(newdate.getMinutes() != 10 && newdate.getMinutes() != 20 && newdate.getMinutes() != 30 && newdate.getMinutes() != 40 && newdate.getMinutes() != 50 && newdate.getMinutes() != 60){
+            return "Last send is Today on " + lastdate.getHours() + ":" + lastdate.getMinutes() + ":" + lastdate.getSeconds();
+        }
     }
 
-    client.post('statuses/update', { status: message }).then(tweet => {
-        console.log(tweet.text);
-        console.log(" ");
-      }).catch(console.error);
+    case "minutes": {
+        if(newdate.getMinutes != 5 && newdate.getMinutes != 3 && newdate.getMinutes != 1 && newdate.getMinutes() != 0){
+            return "Last send is Today on " + lastdate.getHours() + ":" + lastdate.getMinutes() + ":" + lastdate.getSeconds();
+        }
 
-    files.setValue('lastsend', new Date());
-    return "Tweet send!";
-}
-
-var getType = function (value){
-    try {
-        return (new Function("return " + value + ";"))();
-    } catch(e) {
-        return value;
+        if(d == 0){
+            files.setValue("lastsend", "finished");
+            tweet("Cyberpunk 2077 shlould be out now!");
+            return "Last Tweet send!"
+        }
     }
+  }
+
+    tweet(distance[0] + " " + distance[1] + " " + message);
+  files.setValue("lastsend", new Date());
+  return "Tweet send!";
 };
+
+var getType = function (value) {
+  try {
+    return new Function("return " + value + ";")();
+  } catch (e) {
+    return value;
+  }
+};
+
+function tweet (message){
+    client
+    .post("statuses/update", {
+      status: message,
+    })
+    .then((tweet) => {
+      console.log(tweet.text);
+      console.log(" ");
+    })
+    .catch(console.error);
+}
--- server.js
+++ server.js
@@ -12,30 +12,42 @@ const files = require('./modules/files');
 app.use(cors());
 app.use(express.json());
 
+let lastconsole = undefined;
+
 function getDifference(){
     let finish = new Date("2020-11-19");
     let now = new Date();
 
-    var days = Math.round((finish.getTime() - now.getTime()) / (1000 * 3600 * 24));
+    var days = Math.ceil((finish.getTime() - now.getTime()) / (1000 * 3600 * 24));
     var hours = days / 24;
     var minutes = hours / 60;
 
     if(days > 0){
-        return days + " Days";
+        return [days, "Days"];
     } else if(hours > 0){
-        return hours + " Hours";
+        return [hours, "Hours"];
     } else if(minutes > 0){
-        return minutes + " Minutes";
+        return [minutes, "Minutes"];
     } else {
         return false;
     }
 }
 
-var job = scheduler.scheduleJob('*/5 * * * *', () => {
-    console.log(twitter.sendTweet(getDifference() + " until Cyberpunk 2077 release!"));
+var job = scheduler.scheduleJob('*/1 * * * *', () => {
+    if(!getDifference()){
+        return;
+    }
+
+    let log = twitter.sendTweet(getDifference(), "until Cyberpunk 2077 release!");
+
+    if(lastconsole == undefined || lastconsole !== log){
+        lastconsole = log;
+        console.log(chalk.blue(new Date().getHours() +":" + new Date().getMinutes() + ":" + new Date().getSeconds() + "> " +log));
+    }
+    
 });
 
 
 let listener = app.listen(5000, () => {
-    console.log(chalk.cyan(new Date().getTime() + "> Listening on port: " + listener.address().port));
+    console.log(chalk.cyan(new Date().getHours() +":" + new Date().getMinutes() + ":" + new Date().getSeconds() + "> Listening on port: " + listener.address().port));
 });
